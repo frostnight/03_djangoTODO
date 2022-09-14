876dcb05f01e236c70ec752215ce24cf5fbd8b9f
frostnight <frostnightsong@gmail.com>
2022-09-14 14:47:59 +0900

    drf todo 마무리

--- drf_todo/urls.py
+++ drf_todo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from drf_todo.views import TodosAPIView
+from drf_todo.views import TodosAPIView, TodoAPIView, DoneTodosAPIView, DoneTodoAPIView
 
 urlpatterns = [
     path('', TodosAPIView.as_view()),
+    path('<int:pk>', TodoAPIView.as_view()),
+    path('done', DoneTodosAPIView.as_view()),
+    path('done/<int:pk>', DoneTodoAPIView.as_view())
 ]
